8e63e59ab9f586f4e111453e5b081e9bf03e9d72
Lucas <lcsavb@gmail.com>
2025-07-19 23:36:07 +0200

    consolidadte js files

--- static/autocusto/js/mascaras.js
+++ static/autocusto/js/mascaras.js
@@ -5,4 +5,31 @@ $('#id_peso').mask('000', {reverse:true});
 $('#id_cpf_paciente').mask('000.000.000-00', {reverse: false});
 $('#telefone').mask('(00) 0000.0000', {reverse:false});
 $('#cep').mask('00000-000');
+
+// Apply mask to any date field that might be dynamically created
+$(document).on('focus', 'input[name="data_1"], input[id*="data_1"]', function() {
+    if (!$(this).hasClass('masked')) {
+        $(this).mask('00/00/0000');
+        $(this).addClass('masked');
+    }
+});
+
+// Apply masks immediately when document is ready for any existing fields
+setTimeout(function() {
+    $('input[name="data_1"], input[id*="data_1"]').each(function() {
+        if (!$(this).hasClass('masked')) {
+            $(this).mask('00/00/0000');
+            $(this).addClass('masked');
+        }
+    });
+}, 100);
+
+// Masks for the new search form
+$('.cpf-mask').mask('000.000.000-00', {reverse: false});
+$('.cid-mask').mask('A00.0', {
+    translation: {
+        'A': {pattern: /[A-Z]/},
+        '0': {pattern: /[0-9]/}
+    }
+});
 });
